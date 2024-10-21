7ae028efef7a949afdc79652427e11d64e832c7f
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-21 19:27:26 -0300

    Definición de clase

---
+++ dominio/Resultado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Resultado
+    {
+        public string DNI { get; set; }
+        public string Email { get; set; }
+        public List<TiempoPorDisciplina> Tiempo { get; set; }
+    }
+}
