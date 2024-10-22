af6994bc5cc75c1840ccdd038970b5a04e464ea1
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-22 20:34:15 -0300

    Funcionalidad para obtener usuarios cargados en DB

--- negocio/UsuarioNegocio.cs
+++ negocio/UsuarioNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,46 @@ namespace negocio
 {
     public class UsuarioNegocio
     {
+        private AccesoDatos DB;
+        private Usuario Usuario;
+        List<Usuario> Usuarios;
+        public UsuarioNegocio()
+        {
+            DB = new AccesoDatos();
+            Usuario = new Usuario();
+            Usuarios = new List<Usuario>();
+        }
+        public List<Usuario> Listar()
+        {
+            try
+            {
+                DB.setConsulta("SELECT Us.IDUsuario, Us.Nombre, Us.Apellido, Us.DNI, US.CorreoElectronico, US.Contrasenia, US.FechaNacimiento ,DATEDIFF(YEAR, US.FechaNacimiento, GETDATE()) AS Edad, R.IDRol, R.Rol FROM Usuario AS Us INNER JOIN Rol AS R ON Us.IDrol = R.IDRol");
+                DB.ejecutarLectura();
+
+                while (DB.Lector.Read())
+                {
+                    Usuario.IdUsuario = (int)(Int64)DB.Lector["IDUsuario"];
+                    Usuario.Nombre = (string)DB.Lector["Nombre"];
+                    Usuario.Apellido = (string)DB.Lector["Apellido"];
+                    Usuario.Dni = (string)DB.Lector["DNI"];
+                    Usuario.CorreoElectronico = (string)DB.Lector["CorreoElectronico"];
+                    Usuario.FechaNacimiento = (DateTime)DB.Lector["FechaNacimiento"];
+                    Usuario.Contrasenia = (string)DB.Lector["Contrasenia"];
+                    Usuario.Rol.IdRol = (Int16)DB.Lector["IDRol"];
+                    Usuario.Rol.Descripcion = (string)DB.Lector["Rol"];
+
+                    Usuarios.Add(Usuario);
+                }
+                return Usuarios;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DB.cerrarConexion();
+            }
+        }
     }
 }
