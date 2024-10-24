e7d73d615ae4627579af7a012027ef385fe4d860
Facundo Avalos <facundotaraujo@hotmail.com>
2024-10-24 16:42:38 -0300

    Definición de clase

---
+++ dominio/Direccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Direccion
+    {
+        public int ID { get; set; }
+        public string Calle { get; set; }
+        public string Altura { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
---
+++ dominio/Ubicacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Ubicacion
+    {
+        public Ciudad Ciudad { get; set; } 
+        public Pais Pais { get; set; }
+        public Direccion Direccion {get; set;}
+    }
+}
