5a9c543f5885574ce4d6007814a331314cc708fa
Martin Meny <martin.meny@alumnos.frgp.utn.edu.ar>
2024-10-25 11:30:09 -0300

    se agregan controles asp a ventana Log In

--- TPC/LogIn.aspx
+++ TPC/LogIn.aspx
@@ -1,6 +1,23 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="LogIn.aspx.cs" Inherits="TPC.LogIn" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Log In</h1>
+    <div>
+        <h1>Log In</h1>
+    </div>
+    <section>
+        <div>
+            <asp:Label ID="lblUsuario" runat="server" Text="Ingrese su usuario:"></asp:Label>
+            <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="lblContrasena" runat="server" Text="Ingrese su contraseña:"></asp:Label>
+            <asp:TextBox ID="txtContrasena" runat="server" TextMode="Password"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="btnAceptar" runat="server" Text="Aceptar"/>
+        </div>
+    </section>
+
 </asp:Content>
--- TPC/LogIn.aspx.designer.cs
+++ TPC/LogIn.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace TPC
 
     public partial class LogIn
     {
+
+        /// <summary>
+        /// Control lblUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuario;
+
+        /// <summary>
+        /// Control txtUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtUsuario;
+
+        /// <summary>
+        /// Control lblContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContrasena;
+
+        /// <summary>
+        /// Control txtContrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContrasena;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
     }
 }
