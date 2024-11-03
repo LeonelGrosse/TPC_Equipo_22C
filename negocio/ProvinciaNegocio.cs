c13e3bd14102ab492f1eed30af92140f3af02059
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-03 14:56:53 -0300

    Implementación de clase + método para obtener provincias de DB

---
+++ negocio/ProvinciaNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ProvinciaNegocio
+    {
+        private AccesoDatos Datos;
+        private List<Provincia> Provincias; 
+
+        public ProvinciaNegocio()
+        {
+            Datos = new AccesoDatos();
+            Provincias = new List<Provincia>();
+        }
+
+        public List<Provincia> ObtenerProvincias()
+        {
+            try
+            {
+                Datos.SetStoredProcedure("SP_OBTENER_PROVINCIAS");
+                Datos.ejecutarLectura();
+
+                while (Datos.Lector.Read())
+                {
+                    Provincia provincia = new Provincia();
+                    provincia.ID = (int)(Int64)Datos.Lector["ID"];
+                    provincia.Nombre = (string)Datos.Lector["Nombre"];
+
+                    Provincias.Add(provincia);
+                }
+
+                return Provincias;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Datos.cerrarConexion();
+            }
+        }
+
+    }
+}
