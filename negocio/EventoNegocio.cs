15bda5644f8aa60d4d8f160da2a1a8eca9317ab7
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-03 14:55:23 -0300

    Implementé la funcionalidad para insertar un evento en base de datos + cree una propiedad de clase datos para reutilizar en diferentes métodos

--- negocio/EventoNegocio.cs
+++ negocio/EventoNegocio.cs
@@ -12,49 +12,47 @@ namespace negocio
 {
     public class EventoNegocio
     {
-        List<Evento> lista = new List<Evento>();
-        AccesoDatos datos = new AccesoDatos();
+        List<Evento> Lista = new List<Evento>();
+        AccesoDatos Datos = new AccesoDatos();
 
-        public List<Evento> listar()
+        public List<Evento> Listar()
         {
             string select = "SELECT e.IDEvento, e.Nombre, e.FechaEvento, c.Nombre as Ciudad, e.CostoInscripcion, e.Estado, e.EdadMinima, e.CuposDisponibles, i.ImgURL, d.Disciplina ";
             string from = "FROM Evento e INNER JOIN Imagen_x_Evento ie On ie.IDEvento = e.IDEvento INNER JOIN Imagen i On i.ID = ie.IDImagen INNER JOIN Disciplina_x_Evento de On de.IDEvento =e.IDEvento INNER JOIN Disciplina d On d.IDDisciplina = de.IDDisciplina INNER JOIN Ciudad c On c.IDCiudad = e.Ubicacion";
 
             try
             {
-                datos.setConsulta(select + from);
-                datos.ejecutarLectura();
+                Datos.setConsulta(select + from);
+                Datos.ejecutarLectura();
 
-                while (datos.Lector.Read())
+                while (Datos.Lector.Read())
                 {
                     Evento aux = new Evento();
-                    aux.IdEvento = (int)(Int64)datos.Lector["IDEvento"];
-                    aux.Nombre = (string)datos.Lector["Nombre"];
-                    aux.FechaEvento = (DateTime)datos.Lector["FechaEvento"];
+                    aux.IdEvento = (int)(Int64)Datos.Lector["IDEvento"];
+                    aux.Nombre = (string)Datos.Lector["Nombre"];
+                    aux.FechaEvento = (DateTime)Datos.Lector["FechaEvento"];
 
                     aux.Ubicacion = new Ubicacion();
-                    aux.Ubicacion.Ciudad.Nombre = (string)datos.Lector["Ciudad"];
+                    aux.Ubicacion.Ciudad.Nombre = (string)Datos.Lector["Ciudad"];
 
-                    aux.CostoInscripcion = (decimal)datos.Lector["CostoInscripcion"];
+                    aux.CostoInscripcion = (decimal)Datos.Lector["CostoInscripcion"];
 
-                    aux.Estado = (char)datos.Lector["Estado"];
-                    aux.RangoEdad = (int)datos.Lector["EdadMinima"];
-                    aux.CuposDisponibles = (int)datos.Lector["CuposDisponibles"];
+                    //aux.Estado = (char)Datos.Lector["Estado"];
+                    aux.EdadMinima = (int)Datos.Lector["EdadMinima"];
+                    aux.CuposDisponibles = (int)Datos.Lector["CuposDisponibles"];
 
                     aux.Disciplina = new Disciplina();
-                    aux.Disciplina.Descripcion = (string)datos.Lector["Disciplina"];
+                    aux.Disciplina.Descripcion = (string)Datos.Lector["Disciplina"];
 
                     aux.Imagen = new Imagen();
-                    if (!(datos.Lector["ImgURL"] is DBNull))
+                    if (!(Datos.Lector["ImgURL"] is DBNull))
                     {
-                        aux.Imagen.URL = (string)datos.Lector["ImgURL"];
+                        aux.Imagen.URL = (string)Datos.Lector["ImgURL"];
                     }
 
-                    lista.Add(aux);
+                    Lista.Add(aux);
                 }
-
-                return lista;
-
+                return Lista;
             }
             catch (Exception ex)
             {
@@ -63,7 +61,30 @@ namespace negocio
             }
             finally
             {
-                datos.cerrarConexion();
+                Datos.cerrarConexion();
+            }
+        }
+
+        public void RegistrarEvento(Evento evento)
+        {
+            try
+            {
+                Datos.SetStoredProcedure("SP_INSERTAR_EVENTO");
+                Datos.setParametro("@CALLE", evento.Ubicacion.Direccion.Calle);
+                Datos.setParametro("@ALTURA", evento.Ubicacion.Direccion.Altura);
+                Datos.setParametro("@ID_CIUDAD", evento.Ubicacion.Ciudad.IdCiudad);
+                Datos.setParametro("@NOMBRE", evento.Nombre);
+                Datos.setParametro("@FECHA", evento.FechaEvento);
+                Datos.setParametro("@COSTO_INSCRIPCION", evento.CostoInscripcion);
+                Datos.setParametro("@ESTADO", evento.Estado);
+                Datos.setParametro("@EDAD_MINIMA", evento.EdadMinima);
+                Datos.setParametro("@EDAD_MAXIMA", evento.EdadMaxima);
+                Datos.setParametro("@CUPOS_DISPONIBLES", evento.CuposDisponibles);
+                Datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
