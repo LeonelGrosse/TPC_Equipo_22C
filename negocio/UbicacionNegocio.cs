fcbd22fddf27d28a80967840677d7c0c0f420ecf
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-06 21:47:04 -0300

    Esqueleto BM Ubicacion

---
+++ negocio/UbicacionNegocio.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class UbicacionNegocio
+    {
+		private AccesoDatos Datos;
+
+		public UbicacionNegocio()
+		{
+			Datos = new AccesoDatos();
+		}
+        public bool ModificarDireccion() {
+			try
+			{
+				return true;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			finally
+			{
+				Datos.cerrarConexion();
+			}
+        }
+
+		public bool ModificarCiudad()
+		{
+			try
+			{
+				return true;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+			finally
+			{
+				Datos.cerrarConexion();
+			}
+		}
+
+        public bool ModificarProvincia()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                Datos.cerrarConexion();
+            }
+        }
+
+		public bool BajaLogicaCiudad()
+		{
+			try
+			{
+				return true;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+        public bool BajaLogicaPais()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
