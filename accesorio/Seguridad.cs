8bce6b78fc7d330ecfaacba0fab45a18f6f2967a
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-11 09:19:22 -0300

    Agrego clase Seguridad para manejar los accesos por roles

---
+++ accesorio/Seguridad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace accesorio
+{
+    public static class Seguridad
+    {
+        public static Usuario UsuarioLogueado;
+        public static bool SesionActiva(object usuario)
+        {
+            UsuarioLogueado = usuario != null ? (Usuario)usuario : null;
+
+            if (UsuarioLogueado != null && UsuarioLogueado.Rol.IdRol != 0)
+                return true;
+
+            return false;
+        }
+        public static bool RestringirAcceso(object usuario, Roles rol)
+        {
+            UsuarioLogueado = (usuario != null ? (Usuario)usuario : null);
+
+            if (UsuarioLogueado != null && UsuarioLogueado.Rol.IdRol != (int)rol)
+                return true;
+
+            return false;
+        }
+    }
+}
