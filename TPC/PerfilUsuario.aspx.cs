04652c876383a467bb69d647c16809d5d8ba907f
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-13 20:07:20 -0300

    Funcionalidad básica de botones: modificar, confirmar cambios y cancelar + método para habilitar inputs

--- TPC/PerfilUsuario.aspx.cs
+++ TPC/PerfilUsuario.aspx.cs
@@ -40,12 +40,51 @@ namespace TPC
             if (usuario.Imagen.ID != 0)
                 UrlImagen.ImageUrl = usuario.Imagen.URL;
         }
+        protected void btn_modificar_Click(object sender, EventArgs e)
+        {
+            HabilitarCampos();
+            btn_modificar.Visible = false;
+            btn_confirmar.Visible = true;
+            btn_cancelar.Visible = true;
         }
 
-        public void CargarDatosUsuario(string email)
+        protected void btn_confirmar_Click(object sender, EventArgs e)
         {
-            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
-            Usuario usuario = usuarioNegocio.cargar(email);
+            AgregarValidacionesInputs();
+            Page.Validate();
+
+            if (!Page.IsValid)
+                return;
+
+            if (txtNombre.Text != Seguridad.UsuarioLogueado.Nombre)
+                usuarioNegocio.ModificarEscalar("Nombre", Seguridad.UsuarioLogueado.IdUsuario, txtNombre.Text);
+
+            if (txtApellido.Text != Seguridad.UsuarioLogueado.Apellido)
+                usuarioNegocio.ModificarEscalar("Apellido", Seguridad.UsuarioLogueado.IdUsuario, txtApellido.Text);
+
+            if (txtEmail.Text != Seguridad.UsuarioLogueado.CorreoElectronico)
+                usuarioNegocio.ModificarEscalar("CorreoElectronico", Seguridad.UsuarioLogueado.IdUsuario, txtEmail.Text);
+
+            if (txtPassword.Text != Seguridad.UsuarioLogueado.Contrasenia)
+                usuarioNegocio.ModificarEscalar("Contrasena", Seguridad.UsuarioLogueado.IdUsuario, txtPassword.Text);
+        }
+
+        protected void btn_cancelar_Click(object sender, EventArgs e)
+        {
+            CargarDatosUsuario();
+            HabilitarCampos(true);
+            btn_modificar.Visible = true;
+            btn_confirmar.Visible = false;
+            btn_cancelar.Visible = false;
+        }
+
+        private void HabilitarCampos(bool estado = false)
+        {
+            txtNombre.ReadOnly = estado;
+            txtApellido.ReadOnly = estado;
+            txtEmail.ReadOnly = estado;
+            txtPassword.ReadOnly = estado;
+        }
 
             txtNombreUsuario.Text = " " + usuario.Nombre + " " +usuario.Apellido;
             txtDniUsuario.Text = " " + usuario.Dni;
