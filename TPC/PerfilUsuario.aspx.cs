0db1c8156e72b679fad48762f0051ec2db529ba2
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-13 20:04:07 -0300

    Implementación de clase + carga de datos desde DB en form

--- TPC/PerfilUsuario.aspx.cs
+++ TPC/PerfilUsuario.aspx.cs
@@ -11,8 +11,35 @@ namespace TPC
 {
     public partial class PerfilUsuario : System.Web.UI.Page
     {
+        private UsuarioNegocio usuarioNegocio;
+
+        public PerfilUsuario()
+        {
+            usuarioNegocio = new UsuarioNegocio();
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Seguridad.UsuarioLogueado == null)
+                Response.Redirect("Default.aspx");
+
+            if (!IsPostBack)
+                CargarDatosUsuario();
+        }
+
+        public void CargarDatosUsuario()
+        {
+            Usuario usuario = Seguridad.UsuarioLogueado;
+
+            txtNombre.Text = usuario.Nombre;
+            txtApellido.Text = usuario.Apellido;
+            txtDni.Text = usuario.Dni;
+            txtEmail.Text = usuario.CorreoElectronico;
+            txtFechaNacimiento.Text = usuario.FechaNacimiento.ToString("d"); // Fecha corta
+            txtPassword.Text = usuario.Contrasenia;
+            if (usuario.Imagen.ID != 0)
+                UrlImagen.ImageUrl = usuario.Imagen.URL;
+        }
         }
 
         public void CargarDatosUsuario(string email)
