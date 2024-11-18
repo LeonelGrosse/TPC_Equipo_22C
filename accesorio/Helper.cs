87b246ca1330cc919f8a675286e1516081bf9cda
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-18 16:27:15 -0300

    Definición de clase + método estatico p/cargar dropdown

---
+++ accesorio/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace accesorio
+{
+    static public class Helper
+    {
+        static void CargarDropDown<T>(DropDownList dropDown, string dataValueField, string dataTextField, IEnumerable<T> lista)
+        {
+            dropDown.DataSource = lista;
+            dropDown.DataValueField = dataValueField;
+            dropDown.DataTextField = dataTextField;
+            dropDown.DataBind();
+        }
+    }
+}
