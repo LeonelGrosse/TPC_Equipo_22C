b4051039c09ea367fb5721a4b5ff31940f253323
Facundo Avalos <facundotaraujo@hotmail.com>
2024-11-21 01:07:03 -0300

    Implementación de listado por id + método de filtrado

--- negocio/EventoNegocio.cs
+++ negocio/EventoNegocio.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using accesorio;
 using dominio;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -16,11 +17,13 @@ namespace negocio
         List<Evento> Lista = new List<Evento>();
         AccesoDatos Datos = new AccesoDatos();
 
-        public List<Evento> Listar()
+        public List<Evento> Listar(int idEvento = 0)
         {
             try
             {
                 Datos.SetStoredProcedure("SP_LISTA_EVENTO");
+                if (idEvento != 0)
+                    Datos.setParametro("@IDEvento", idEvento);
                 Datos.ejecutarLectura();
 
                 while (Datos.Lector.Read())
@@ -79,9 +82,11 @@ namespace negocio
                 while (datos.Lector.Read())
                 {
                     Disciplina auxiliarDisciplina = new Disciplina();
-                    auxiliarDisciplina.IdDisciplina = (int)(Int64)datos.Lector["IDDisciplina"];
-                    auxiliarDisciplina.Descripcion = (string)datos.Lector["Disciplina"];
+                    auxiliarDisciplina.ID = (int)(Int64)datos.Lector["IDDisciplina"];
+                    auxiliarDisciplina.Nombre = (string)datos.Lector["Disciplina"];
                     auxiliarDisciplina.Distancia = (decimal)datos.Lector["Distancia"];
+
+                    evento.Disciplina.Add(auxiliarDisciplina);
                 }
             }
             catch (Exception ex)
@@ -125,7 +130,7 @@ namespace negocio
                     aux.EdadMinima = (int)(byte)Datos.Lector["EdadMinima"];
                     aux.CuposDisponibles = (int)Datos.Lector["CuposDisponibles"];
 
-                    aux.Disciplina.Add(new Disciplina { Descripcion = (string)Datos.Lector["Disciplina"] });
+                    CargarDisciplinasDeEvento(aux);
 
                     if (!(Datos.Lector["ImgURL"] is DBNull))
                     {
@@ -178,7 +183,7 @@ namespace negocio
                 Datos.LimpiarComando();
                 Datos.setConsulta("INSERT INTO Disciplina_x_Evento(IDEvento, IDDisciplina, Distancia)VALUES(@ID_EVENTO, @ID_DISCIPLINA, @DISTANCIA)");
                 Datos.setParametro("@ID_EVENTO", IDEvento);
-                Datos.setParametro("@ID_DISCIPLINA", disciplina.IdDisciplina);
+                Datos.setParametro("@ID_DISCIPLINA", disciplina.ID);
                 Datos.setParametro("@DISTANCIA", disciplina.Distancia);
 
                 return Datos.EjecutarAccion();
@@ -284,6 +289,7 @@ namespace negocio
         {
             try
             {
+                Datos.LimpiarComando();
                 Datos.setConsulta("INSERT INTO Usuario_x_Evento (IDEvento, IDUsuario) VALUES (@idEvento, @idUsuario)");
                 Datos.setParametro("@idEvento", idEvento);
                 Datos.setParametro("@idUsuario", idUsuario);
@@ -291,8 +297,68 @@ namespace negocio
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                Datos.cerrarConexion();
+            }
+        }
 
-                throw ex;
+        public List<Evento> Filtrar(string campo, int criterio)
+        {
+            Filtro filtro = new Filtro();
+
+            try
+            {
+                string query = "Select E.IDEvento, E.Nombre, E.FechaEvento, E.CostoInscripcion, E.EdadMinima, E.EdadMaxima, E.CuposDisponibles, E.Estado, P.ID as IDProvincia, P.Nombre as Provincia, C.IDCiudad, C.Nombre as Ciudad, Dir.ID, Dir.Calle, Dir.Altura, IxE.IDImagen, IxE.ImgURL FROM Evento AS E left join Imagen_x_Evento AS IxE On IxE.IDEvento = E.IDEvento Inner join Ubicacion AS U On U.IDUbicacion = E.Ubicacion Inner join Ciudad AS C On C.IDCiudad = u.IDCiudad Inner join Provincia AS P On P.ID = C.IDProvincia Inner join Direccion AS Dir On Dir.ID = U.IDDireccion Where E.Estado = 'D'"
+;
+                filtro.SetearQueryBase(query);
+                filtro.AplicarCriterioAQuery(campo, criterio);
+
+                Datos.setConsulta(filtro.query);
+                Datos.ejecutarLectura();
+
+                while (Datos.Lector.Read())
+                {
+                    Evento aux = new Evento();
+                    aux.IdEvento = (int)(Int64)Datos.Lector["IDEvento"];
+                    aux.Nombre = (string)Datos.Lector["Nombre"];
+                    aux.FechaEvento = (DateTime)Datos.Lector["FechaEvento"];
+
+                    aux.Ubicacion = new Ubicacion();
+                    aux.Ubicacion.Ciudad.Nombre = (string)Datos.Lector["Ciudad"];
+                    aux.Ubicacion.Ciudad.Provincia.ID = (int)(Int64)Datos.Lector["IDProvincia"];
+                    aux.Ubicacion.Ciudad.Provincia.Nombre = (string)Datos.Lector["Provincia"];
+                    aux.Ubicacion.Direccion.ID = (short)Datos.Lector["ID"];
+                    aux.Ubicacion.Direccion.Calle = (string)Datos.Lector["Calle"];
+                    aux.Ubicacion.Direccion.Altura = (string)Datos.Lector["Altura"];
+
+                    aux.CostoInscripcion = (decimal)Datos.Lector["CostoInscripcion"];
+
+                    aux.Estado = char.Parse(Datos.Lector["Estado"].ToString());
+                    aux.EdadMinima = (int)(byte)Datos.Lector["EdadMinima"];
+                    aux.EdadMaxima = (int)(byte)Datos.Lector["EdadMaxima"];
+                    aux.CuposDisponibles = (int)Datos.Lector["CuposDisponibles"];
+
+                    CargarDisciplinasDeEvento(aux);
+
+                    if (!(Datos.Lector["ImgURL"] is DBNull))
+                    {
+                        aux.Imagen.URL = (string)Datos.Lector["ImgURL"];
+                    }
+                    else aux.Imagen.URL = "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image-300x225.png";
+
+                    Lista.Add(aux);
+                }
+
+                return Lista;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
             }
             finally
             {
